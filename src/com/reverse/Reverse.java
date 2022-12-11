f193694e60817c4f9370c32800bed02d56aae05e
dinesh1596 <dinesharasu1596@gmaol.com>
2022-12-10 23:56:21 -0500

    new commit

---
+++ src/com/reverse/Reverse.java
@@ -0,0 +1,15 @@
+package com.reverse;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str ="dinesh";
+		String str1 = "";
+		for(int i=str.length()-1;i>=0;i--) {
+			str1=str1+str.charAt(i);
+		}
+System.out.println(str1);
+	}
+
+}
