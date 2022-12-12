0d1d1db31e29a2e9816a5c2401964ea743e71760
sathiyanila <ssmorthy32@gmail.com>
2022-12-12 03:55:59 -0500

    2

--- src/com/reverse/Reverse.java
+++ src/com/reverse/Reverse.java
@@ -10,6 +10,18 @@ public class Reverse {
 			str1=str1+str.charAt(i);
 		}
 System.out.println(str1);
+	
+	
+	
+	}
+	private void m1() {
+		String str ="dinesh";
+		String str1 = "";
+		for(int i=str.length()-1;i>=0;i--) {
+			str1=str1+str.charAt(i);
+		}
+System.out.println(str1);
+
 	}
 
 }
